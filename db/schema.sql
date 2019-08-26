c015dc99862df9d790b411d84429faa71306c4ee
Carolina Cavalcanti <carolinapc@gmail.com>
2019-08-26 19:10:44 -0400

    changed the schema to match the heroku standard

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
